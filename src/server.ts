71a35f2b481fa2c9b0dac546890735650ec3e289
rakitenkogermans <rakitenko.germans@gmail.com>
2022-07-24 20:05:25 +0300

    connect db

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response } from 'express';
 import notFoundMiddleware from './middleware/not-found';
 import errorHandlerMiddleware from './middleware/error-handler';
 import dotenv from 'dotenv';
+import connectDB from './db/connect';
 
 dotenv.config();
 
@@ -9,11 +10,21 @@ const PORT = process.env.PORT || 5000;
 const app: Express = express();
 
 app.get('/', (req: Request, res: Response) => {
-    throw Error('error');
     res.send('Welcome.');
 });
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-app.listen(PORT, () => console.log(`App is listening on port: ${PORT}`));
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URL || '');
+        app.listen(PORT, () =>
+            console.log(`App is listening on port: ${PORT}`)
+        );
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+start();
