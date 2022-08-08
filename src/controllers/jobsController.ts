b632a1ed2c789a3ff8df2c95fe7b2db7ea4e4898
rakitenkogermans <rakitenko.germans@gmail.com>
2022-08-08 19:32:48 +0300

    pagination functionality on the server

--- src/controllers/jobsController.ts
+++ src/controllers/jobsController.ts
@@ -39,7 +39,7 @@ const deleteJob: RequestHandler = async (req, res, next) => {
 };
 
 const getAllJobs = async (
-    req: Request<{}, {}, {}, { status: string; jobType: string; sort: string; search: string }>,
+    req: Request<{}, {}, {}, { status: string; jobType: string; sort: string; search: string; page: number; limit: number }>,
     res: Response,
     next: NextFunction
 ) => {
@@ -76,8 +76,15 @@ const getAllJobs = async (
             result = result.sort('-position');
         }
 
-        const jobs = await result;
-        res.status(StatusCodes.OK).json({ jobs, totalJobs: jobs.length, numOfPages: 1 });
+        const page = Number(req.query.page) || 1;
+        const limit = Number(req.query.limit) || 10;
+        const skip = (page - 1) * limit;
+        result = result.skip(skip).limit(limit);
+
+        const [jobs, totalJobs] = await Promise.all([result, Job.countDocuments(queryObject)]);
+        const numOfPages = Math.ceil(totalJobs / limit);
+
+        res.status(StatusCodes.OK).json({ jobs, totalJobs, numOfPages });
     } catch (err) {
         next(err);
     }
