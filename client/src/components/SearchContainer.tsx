1d46e9ac95964d636a222d1e9ade6dc1ed067441
rakitenkogermans <rakitenko.germans@gmail.com>
2023-01-19 20:02:35 +0200

    search input debounce

--- client/src/components/SearchContainer.tsx
+++ client/src/components/SearchContainer.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, FC, MouseEvent } from 'react';
+import { ChangeEvent, FC, MouseEvent, useMemo, useState } from 'react';
 
 import { FormRow, FormRowSelect } from '.';
 import { useAppContext } from '../context/appContext';
@@ -8,9 +8,9 @@ import { SearchAll } from '../types/types';
 type SearchContainerProps = {};
 
 const SearchContainer: FC<SearchContainerProps> = () => {
+    const [localSearch, setLocalSearch] = useState('');
     const {
         isLoading,
-        search,
         searchStatus,
         searchType,
         sort,
@@ -22,7 +22,6 @@ const SearchContainer: FC<SearchContainerProps> = () => {
     } = useAppContext();
 
     const handleSearch = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-        if (isLoading) return;
         const name = e.target.name;
         const value = e.target.value;
         handleChange(name, value);
@@ -30,15 +29,36 @@ const SearchContainer: FC<SearchContainerProps> = () => {
 
     const handleSubmit = (e: MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
+        setLocalSearch('');
         clearFilters();
     };
+
+    const debounce = () => {
+        let timeoutID: NodeJS.Timeout;
+        return (e: ChangeEvent<HTMLInputElement>) => {
+            setLocalSearch(e.target.value);
+            clearTimeout(timeoutID);
+            timeoutID = setTimeout(() => {
+                const name = e.target.name;
+                const value = e.target.value;
+                handleChange(name, value);
+            }, 1000);
+        };
+    };
+
+    const optimizedDebounce = useMemo(() => debounce(), []);
 
     return (
         <Wrapper>
             <form className="form">
                 <h4>search form</h4>
                 <div className="form-center">
-                    <FormRow type="text" name="search" value={search} handleChange={handleSearch} />
+                    <FormRow
+                        type="text"
+                        name="search"
+                        value={localSearch}
+                        handleChange={optimizedDebounce}
+                    />
                     <FormRowSelect
                         labelText="job status"
                         name="searchStatus"
